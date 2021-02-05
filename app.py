284c223debfb91da681775f431da085055b92602
lilian <lilian.pacaud1@gmail.com>
2021-02-05 18:26:01 +0100

    test post & get

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello world'
+
+@app.route('/testGet', methods=['GET'])
+def testGet():
+    return 'Test GET'
+
+@app.route('/testPost', methods=['POST'])
+def testPost():
+    return request.json['test']
+
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+    app.run(debug=True, port=1234, host='0.0.0.0')
